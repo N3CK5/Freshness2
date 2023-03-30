241c9b69ea539e3e6ac6ed0b487a81c6106050cd
Cumer Maxence <maxence.cumer@student-cs.fr>
2023-03-30 19:00:23 +0200

    Clarification du code, création d'une nouvelle classe Results

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from device import Device
 from monitor import Monitor
 from simulation import Simulation
 from Aloha import Aloha
+from results import Results
 import numpy as np
 import math
 
@@ -94,116 +95,12 @@ while(sim.isRunning):
     if(sim.isEnd()):
         sim.end()
 
+#--------------------------RESULTS OF THE SIMULATION--------------------------------#
 
-for i in range(len(devicesList)):
-    print(devicesList[i].p_e)
-    devicesList[i].comms_history = [element[0] for element in devicesList[i].comms_history if len(element) > 1]
-    
-
-for i in range(NB_DEVICES):
-    for j in range(NB_DEVICES):
-        if i > j:
-            ensI = set(devicesList[i].comms_history)
-            ensJ = set(devicesList[j].comms_history)
-            ensI1 = ensI - ensJ
-            ensJ1 = ensJ - ensI
-            lI = list(ensI1)
-            lJ = list(ensJ1)
-            lI.sort()
-            lJ.sort()
-            devicesList[i].comms_history = lI
-            devicesList[j].comms_history = lJ
-
-
-fig, axs = plt.subplots(NB_DEVICES, sharex=True, sharey=True)
-fig.suptitle("Age of Information for each device")
-
-def returnMoyAoIOfADevice(commsHistory):
-    # Uses only the comms_history when each communication is written with the beginning epoch ONLY
-    if len(commsHistory) == 0:
-        return SIM_LIFESPAN
-    sumCommsAoI = commsHistory[0]
-    j = 1
-    for i in range(len(commsHistory)-1):
-        sumCommsAoI += commsHistory[i+1] - commsHistory[i]
-        j+=1
-    sumCommsAoI += SIM_LIFESPAN - commsHistory[-1]
-    j+=1
-    return sumCommsAoI/(len(commsHistory)+1)
-
-def returnMaxAoIOfADevice(commsHistory):
-    # Uses only the comms_history when each communication is written with the beginning epoch ONLY
-    if len(commsHistory) == 0:
-        return SIM_LIFESPAN
-    maxCommsAoI = commsHistory[0]
-    for i in range(len(commsHistory)-1):
-        if maxCommsAoI < commsHistory[i+1] - commsHistory[i]:
-            maxCommsAoI = commsHistory[i+1] - commsHistory[i]
-    maxCommsAoI = max(maxCommsAoI, SIM_LIFESPAN - commsHistory[-1])
-    return maxCommsAoI
-
-def returnMinAoIOfADevice(commsHistory):
-    # Uses only the comms_history when each communication is written with the beginning epoch ONLY
-    if len(commsHistory) == 0:
-        return SIM_LIFESPAN
-    minCommsAoI = commsHistory[0]
-    for i in range(len(commsHistory)-1):
-        if minCommsAoI > commsHistory[i+1] - commsHistory[i]:
-            minCommsAoI = commsHistory[i+1] - commsHistory[i]
-    minCommsAoI = min(minCommsAoI, SIM_LIFESPAN - commsHistory[-1])
-    return minCommsAoI
-
-print("Device   Moy  Max  Min")
-moyAoI = 0
-minAoI = math.inf
-maxAoI = 0
-for i in range(NB_DEVICES):
-    moyAoiDev = returnMoyAoIOfADevice(devicesList[i].comms_history)
-    maxAoIDev = returnMaxAoIOfADevice(devicesList[i].comms_history)
-    minAoIDev = returnMinAoIOfADevice(devicesList[i].comms_history)
-    moyAoI, maxAoI, minAoI = moyAoI + moyAoiDev, max(maxAoI, maxAoIDev), min(minAoI, minAoIDev)
-    # We print here the mean, min and max value of the AoI of each device then of the network
-    print("Device", i, moyAoiDev, maxAoIDev, minAoIDev)
-print("Results:")
-print("Mean of the AoI: ", moyAoI/NB_DEVICES)
-print("Max of the AoI: ", maxAoI)
-print("Min of the AoI: ", minAoI)
-
-
-X = [[] for k in range(NB_DEVICES)]
-Y = [[] for k in range(NB_DEVICES)]
-colorGraphs = {0: 'g-', 1:'b-', 2:'r-',3:'c-', 4:'k-', 5:'m-'}
-
-for k in range(NB_DEVICES):
-    X[k].append(0)
-    Y[k].append(0)
-    if len(devicesList[k].comms_history) == 0:
-        X[k].append(SIM_LIFESPAN)
-        Y[k].append(SIM_LIFESPAN)
-        axs[k].plot(X[k], Y[k], colorGraphs[k%len(colorGraphs)])
-        continue
-    X[k].append(devicesList[k].comms_history[0])
-    Y[k].append(devicesList[k].comms_history[0])
-    i = -1
-    for i in range(len(devicesList[k].comms_history) - 1):
-        X[k].append(devicesList[k].comms_history[i])
-        Y[k].append(0)
-        X[k].append(devicesList[k].comms_history[i+1])
-        Y[k].append(devicesList[k].comms_history[i+1] - devicesList[k].comms_history[i])
-        X[k].append(devicesList[k].comms_history[i+1])
-        Y[k].append(0)
-    if devicesList[k].comms_history[i+1] < SIM_LIFESPAN - SIM_TF:
-        X[k].append(devicesList[k].comms_history[i+1])
-        Y[k].append(0)
-        X[k].append(SIM_LIFESPAN)
-        Y[k].append(SIM_LIFESPAN - devicesList[k].comms_history[i+1])
-    axs[k].plot(X[k], Y[k], colorGraphs[k%len(colorGraphs)])
-
-plt.setp(axs[-1], xlabel='Time (in epochs)')
-for i in range(NB_DEVICES):
-    plt.setp(axs[i], ylabel='AoI (in epochs)')
-
-plt.savefig("AOI_graph.jpg")
+results = Results(devicesList, SIM_LIFESPAN)
+results.modifyCommsHistories()
+results.plotAoIGraphs()
+results.printResultsAoI()
 
 
 #print(mainMonitor.db)
---
+++ results.py
@@ -0,0 +1,140 @@
+import math
+from matplotlib import pyplot as plt
+
+class Results():
+    '''
+    Gives and prints the results
+    '''
+    
+    def __init__(self, devicesList, lifespan):
+        self.devicesList = devicesList
+        self.lifespan = lifespan
+
+    '''
+    Function modifying the communication history of each device to make it simplier to be displayed
+    '''
+    def modifyCommsHistories(self):
+        for i in range(len(self.devicesList)):
+            print(self.devicesList[i].p_e)
+            self.devicesList[i].comms_history = [element[0] for element in self.devicesList[i].comms_history if len(element) > 1]
+
+        for i in range(len(self.devicesList)):
+            for j in range(len(self.devicesList)):
+                if i > j:
+                    ensI = set(self.devicesList[i].comms_history)
+                    ensJ = set(self.devicesList[j].comms_history)
+                    ensI1 = ensI - ensJ
+                    ensJ1 = ensJ - ensI
+                    lI = list(ensI1)
+                    lJ = list(ensJ1)
+                    lI.sort()
+                    lJ.sort()
+                    self.devicesList[i].comms_history = lI
+                    self.devicesList[j].comms_history = lJ
+    
+    '''
+    Function returning the mean value of the AoI of a device
+    '''
+    def returnMoyAoIOfADevice(self, commsHistory):
+        # Uses only the comms_history when each communication is written with the beginning epoch ONLY
+        if len(commsHistory) == 0:
+            return self.lifespan
+        sumCommsAoI = commsHistory[0]
+        j = 1
+        for i in range(len(commsHistory)-1):
+            sumCommsAoI += commsHistory[i+1] - commsHistory[i]
+            j+=1
+        sumCommsAoI += self.lifespan - commsHistory[-1]
+        j+=1
+        return sumCommsAoI/(len(commsHistory)+1)
+    
+    '''
+    Function returning the maximum value of the AoI of a device
+    '''
+    def returnMaxAoIOfADevice(self, commsHistory):
+        # Uses only the comms_history when each communication is written with the beginning epoch ONLY
+        if len(commsHistory) == 0:
+            return self.lifespan
+        maxCommsAoI = commsHistory[0]
+        for i in range(len(commsHistory)-1):
+            if maxCommsAoI < commsHistory[i+1] - commsHistory[i]:
+                maxCommsAoI = commsHistory[i+1] - commsHistory[i]
+        maxCommsAoI = max(maxCommsAoI, self.lifespan - commsHistory[-1])
+        return maxCommsAoI
+    
+    '''
+    Function returning the minimum value of the AoI of a device
+    '''
+    def returnMinAoIOfADevice(self, commsHistory):
+        # Uses only the comms_history when each communication is written with the beginning epoch ONLY
+        if len(commsHistory) == 0:
+            return self.lifespan
+        minCommsAoI = commsHistory[0]
+        for i in range(len(commsHistory)-1):
+            if minCommsAoI > commsHistory[i+1] - commsHistory[i]:
+                minCommsAoI = commsHistory[i+1] - commsHistory[i]
+        minCommsAoI = min(minCommsAoI, self.lifespan - commsHistory[-1])
+        return minCommsAoI
+    
+    '''
+    Function printing the results of the simulation
+    '''
+    def printResultsAoI(self):
+        print("Device   Moy  Max  Min")
+        moyAoI = 0
+        minAoI = math.inf
+        maxAoI = 0
+        for i in range(len(self.devicesList)):
+            moyAoiDev = self.returnMoyAoIOfADevice(self.devicesList[i].comms_history)
+            maxAoIDev = self.returnMaxAoIOfADevice(self.devicesList[i].comms_history)
+            minAoIDev = self.returnMinAoIOfADevice(self.devicesList[i].comms_history)
+            moyAoI, maxAoI, minAoI = moyAoI + moyAoiDev, max(maxAoI, maxAoIDev), min(minAoI, minAoIDev)
+            # We print here the mean, min and max value of the AoI of each device then of the network
+            print("Device", i, moyAoiDev, maxAoIDev, minAoIDev)
+        print("Results:")
+        print("Mean of the AoI: ", moyAoI/len(self.devicesList))
+        print("Max of the AoI: ", maxAoI)
+        print("Min of the AoI: ", minAoI)
+    
+    '''
+    Function ploting the AoI of the devices
+    '''
+    def plotAoIGraphs(self):
+        fig, axs = plt.subplots(len(self.devicesList), sharex=True, sharey=True)
+        fig.suptitle("Age of Information for each device")
+
+        X = [[] for k in range(len(self.devicesList))]
+        Y = [[] for k in range(len(self.devicesList))]
+        #colorGraphs = {0: 'g-', 1:'b-', 2:'r-',3:'c-', 4:'k-', 5:'m-'}
+        colorGraphs = {0: 'g-', 1:'b-', 2:'r-',3:'c-', 4:'orange', 5:'k-', 6:'m-'}
+
+        for k in range(len(self.devicesList)):
+            X[k].append(0)
+            Y[k].append(0)
+            if len(self.devicesList[k].comms_history) == 0:
+                X[k].append(self.lifespan)
+                Y[k].append(self.lifespan)
+                axs[k].plot(X[k], Y[k], colorGraphs[k%len(colorGraphs)])
+                continue
+            X[k].append(self.devicesList[k].comms_history[0])
+            Y[k].append(self.devicesList[k].comms_history[0])
+            i = -1
+            for i in range(len(self.devicesList[k].comms_history) - 1):
+                X[k].append(self.devicesList[k].comms_history[i])
+                Y[k].append(0)
+                X[k].append(self.devicesList[k].comms_history[i+1])
+                Y[k].append(self.devicesList[k].comms_history[i+1] - self.devicesList[k].comms_history[i])
+                X[k].append(self.devicesList[k].comms_history[i+1])
+                Y[k].append(0)
+            if self.devicesList[k].comms_history[i+1] < self.lifespan:
+                X[k].append(self.devicesList[k].comms_history[i+1])
+                Y[k].append(0)
+                X[k].append(self.lifespan)
+                Y[k].append(self.lifespan - self.devicesList[k].comms_history[i+1])
+            axs[k].plot(X[k], Y[k], colorGraphs[k%len(colorGraphs)])
+
+        plt.setp(axs[-1], xlabel='Time (in epochs)')
+        for i in range(len(self.devicesList)):
+            plt.setp(axs[i], ylabel='AoI (in epochs)')
+
+        plt.savefig("AOI_graph.jpg")
