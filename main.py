71a3979f62d15862fec8e4a11727a1707723e6a8
Cumer Maxence <maxence.cumer@student-cs.fr>
2023-03-30 17:30:49 +0200

    Enlever les Tf

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from matplotlib.patches import Rectangle
 from celluloid import Camera
 
 #CONSTANTS
-NB_DEVICES = 200
-SIM_LIFESPAN = 10000
+NB_DEVICES = 5
+SIM_LIFESPAN = 100
 SIM_TF = 3
 ITERATIONS_ALOHA = 60
 
@@ -31,7 +31,7 @@ aloha.run_algorithm(ITERATIONS_ALOHA, eta = 0.5)
 print("result for p_e's : ", aloha.display_pe())
 print("result for lambda_e's : ", aloha.display_lambda())
 
-aloha.plot_lambda(ITERATIONS_ALOHA)
+#aloha.plot_lambda(ITERATIONS_ALOHA)
 
 
 #-------------------------------------------------------------------------#
@@ -71,6 +71,9 @@ def display(activatedDevicesList, camera):
     plt.pause(0.1)    
     camera.snap()
 
+
+#--------------------------SIMULATION OF THE NETWORK--------------------------------#
+
 sim = Simulation(devicesList, SIM_LIFESPAN, SIM_TF)
 sim.start()
 
@@ -80,9 +83,7 @@ while(sim.isRunning):
     sim.predictDevices()
     
     #wait for a number of epoch
-    if not sim.waitToSend(10):
-        sim.activateDevices(mainMonitor)
-    
+    sim.activateDevices(mainMonitor)
     #print(sim.activatedDevicesList)
 
 
@@ -94,87 +95,28 @@ while(sim.isRunning):
     if(sim.isEnd()):
         sim.end()
     
-""" for i in range(len(devicesList)):
-    print(devicesList[i].p_e)
-
-
 for i in range(len(devicesList)):
-    devicesList[i].comms_history = [element for element in devicesList[i].comms_history if len(element) > 1]
+    print(devicesList[i].p_e)
+    devicesList[i].comms_history = [element[0] for element in devicesList[i].comms_history if len(element) > 1]
     
-    print(devicesList[i].comms_history)
 
 for i in range(NB_DEVICES):
     for j in range(NB_DEVICES):
         if i > j:
-            ensI = set([devicesList[i].comms_history[l][0] for l in range(len(devicesList[i].comms_history))])
-            ensJ = set([devicesList[j].comms_history[l][0] for l in range(len(devicesList[j].comms_history))])
+            ensI = set(devicesList[i].comms_history)
+            ensJ = set(devicesList[j].comms_history)
             ensI1 = ensI - ensJ
             ensJ1 = ensJ - ensI
             lI = list(ensI1)
             lJ = list(ensJ1)
             lI.sort()
             lJ.sort()
-            devicesList[i].comms_history = [[element, element + SIM_TF] for element in lI]
-            devicesList[j].comms_history = [[element, element + SIM_TF] for element in lJ]
-            #intersection = ensI.intersection(ensJ)
+            devicesList[i].comms_history = lI
+            devicesList[j].comms_history = lJ
 
-
-    
-def retourneMoyenneAoI(ListeDevices):
-    listeMoyennesAoIParDevice = []
-    for k in range(NB_DEVICES):
-        moy = 0
-        nb = 0
-        if len(ListeDevices[k].comms_history) == 0:
-            listeMoyennesAoIParDevice.append(SIM_LIFESPAN)
-            continue
-        for i in range(len(ListeDevices[k].comms_history) - 1):
-            moy += ListeDevices[k].comms_history[i+1][0] - ListeDevices[k].comms_history[i][1]
-            nb += 1
-        listeMoyennesAoIParDevice.append(moy/nb)
-
-    return sum(listeMoyennesAoIParDevice)/len(listeMoyennesAoIParDevice)
-
-
-def retourneMaxAoI(ListeDevices):
-    listeMaxParDevice = []
-    for k in range(NB_DEVICES):
-        maximum = 0
-        if len(ListeDevices[k].comms_history) == 0:
-            listeMaxParDevice.append(SIM_LIFESPAN)
-            continue
-        for i in range(len(ListeDevices[k].comms_history) - 1):
-            if ListeDevices[k].comms_history[i+1][0] - ListeDevices[k].comms_history[i][1] > maximum:
-                maximum = ListeDevices[k].comms_history[i+1][0] - ListeDevices[k].comms_history[i][1]
-        listeMaxParDevice.append(maximum)
-    return max(listeMaxParDevice)
-
-def retourneMinAoI(ListeDevices):
-    listeMaxParDevice = []
-    for k in range(NB_DEVICES):
-        minimum = math.inf
-        if len(ListeDevices[k].comms_history) == 0:
-            listeMaxParDevice.append(SIM_LIFESPAN)
-            continue
-        for i in range(len(ListeDevices[k].comms_history) - 1):
-            if ListeDevices[k].comms_history[i+1][0] - ListeDevices[k].comms_history[i][1] < minimum :
-                minimum = ListeDevices[k].comms_history[i+1][0] - ListeDevices[k].comms_history[i][1]
-        listeMaxParDevice.append(minimum)
-    return max(listeMaxParDevice)
-
-
-for i in range(NB_DEVICES):
-    print(devicesList[i].comms_history)
-    
 
 fig, axs = plt.subplots(NB_DEVICES, sharex=True, sharey=True)
 fig.suptitle("Age of Information for each device")
-
-for k in range(NB_DEVICES):
-    for i in range(len(devicesList[k].comms_history) - 1):
-        axs[k].plot([devicesList[k].comms_history[i][1], devicesList[k].comms_history[i+1][0]], [0, devicesList[k].comms_history[i+1][0] - devicesList[k].comms_history[i][0]])
-        axs[k].vlines(x = devicesList[k].comms_history[i+1][0], ymin = 0, ymax = devicesList[k].comms_history[i+1][0] - devicesList[k].comms_history[i][0])
-        axs[k].hlines(y = 0, xmin = devicesList[k].comms_history[i][0], xmax = devicesList[k].comms_history[i][1])
 
 
 X = [[] for k in range(NB_DEVICES)]
@@ -189,33 +131,29 @@ for k in range(NB_DEVICES):
         Y[k].append(SIM_LIFESPAN)
         axs[k].plot(X[k], Y[k], colorGraphs[k%len(colorGraphs)])
         continue
-    X[k].append(devicesList[k].comms_history[0][0])
-    Y[k].append(devicesList[k].comms_history[0][0])
+    X[k].append(devicesList[k].comms_history[0])
+    Y[k].append(devicesList[k].comms_history[0])
     i = -1
     for i in range(len(devicesList[k].comms_history) - 1):
-        X[k].append(devicesList[k].comms_history[i][0])
+        X[k].append(devicesList[k].comms_history[i])
         Y[k].append(0)
-        X[k].append(devicesList[k].comms_history[i][1])
-        X[k].append(devicesList[k].comms_history[i+1][0])
+        X[k].append(devicesList[k].comms_history[i+1])
+        Y[k].append(devicesList[k].comms_history[i+1] - devicesList[k].comms_history[i])
+        X[k].append(devicesList[k].comms_history[i+1])
         Y[k].append(0)
-        Y[k].append(devicesList[k].comms_history[i+1][0] - devicesList[k].comms_history[i][0])
-        X[k].append(devicesList[k].comms_history[i+1][0])
-        Y[k].append(0)
-    #if len(devicesList[k].comms_history) == 1:
-    #    X[k].append(SIM_LIFESPAN)
-    #    Y[k].append(0)
-    if devicesList[k].comms_history[i+1][0] < SIM_LIFESPAN - SIM_TF:
-        X[k].append(devicesList[k].comms_history[i+1][0]+SIM_TF-1)
+    if devicesList[k].comms_history[i+1] < SIM_LIFESPAN - SIM_TF:
+        X[k].append(devicesList[k].comms_history[i+1])
         Y[k].append(0)
         X[k].append(SIM_LIFESPAN)
-        Y[k].append(SIM_LIFESPAN - devicesList[k].comms_history[i+1][0])
+        Y[k].append(SIM_LIFESPAN - devicesList[k].comms_history[i+1])
     axs[k].plot(X[k], Y[k], colorGraphs[k%len(colorGraphs)])
 
 plt.setp(axs[-1], xlabel='Time (in epochs)')
 for i in range(NB_DEVICES):
-    plt.setp(axs[i], ylabel='AoI (in epochs)') """
+    plt.setp(axs[i], ylabel='AoI (in epochs)')
 
-#plt.savefig("AOI_graph.jpg")
+plt.savefig("AOI_graph.jpg")
+
 
 #print(mainMonitor.db)
 #animation = snap.animate()
